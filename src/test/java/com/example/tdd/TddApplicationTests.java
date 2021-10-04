a85b532507fb1afd06dd6d4ca2e5020aa44a3855
ys313 <ys3135@gmail.com>
2021-10-04 15:33:31 +0900

    5장 - 큰 테스트를 공략할 수 없다. 그래서 진전을 나타낼수 있는 자그마한 테스트를 만들었다. - 뻔뻔스럽게도 중복을 만들고 조금 고쳐서 테스트를 작성 - 설상가상으로 모델 코드까지 도매금으로 복사하고 수정해서 테스트를 통과했다.

--- src/test/java/com/example/tdd/TddApplicationTests.java
+++ src/test/java/com/example/tdd/TddApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.tdd;
 
 import com.example.tdd.Money.Dollar;
+import com.example.tdd.Money.Franc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -18,6 +19,12 @@ class TddApplicationTests {
         Dollar five = new Dollar(5);
         assertEquals(new Dollar(10),five.times(2));
         assertEquals(new Dollar(15),five.times(3));
+    }
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10),five.times(2));
+        assertEquals(new Franc(15),five.times(3));
     }
 
     @Test
