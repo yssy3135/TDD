b25d313b0de0dc1240667eb2bc78d51521465893
ys313 <ys3135@gmail.com>
2021-10-05 23:47:20 +0900

    8장 - 동일한 메서드(times)의 두변이형 메서드 서명부를 통일 - 공통상위클래스(superclass)로 옮김 - 팩토리 메서드를 도입하여 테스트 코드에서 콘크리트 하위 클래스의 존재 사실을 분리

--- src/main/java/com/example/tdd/Money/Money.java
+++ src/main/java/com/example/tdd/Money/Money.java
@@ -1,11 +1,22 @@
 package com.example.tdd.Money;
 
-public class Money {
+public abstract class Money {
     protected int amount;
-
+    public abstract Money times(int muiltiplier);
 
     public boolean equals(Object object){
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(object.getClass());
     }
+
+    public static Money dollar(int amount){
+        return new Dollar(amount);
+    }
+
+    public static Franc franc(int amount){
+        return new Franc(amount);
+    }
+
+
+
 }
--- src/test/java/com/example/tdd/TddApplicationTests.java
+++ src/test/java/com/example/tdd/TddApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.tdd;
 
 import com.example.tdd.Money.Dollar;
 import com.example.tdd.Money.Franc;
+import com.example.tdd.Money.Money;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,9 +16,9 @@ class TddApplicationTests {
 
     @Test
     public void testMultiplication() {
-        Dollar five = new Dollar(5);
-        assertEquals(new Dollar(10),five.times(2));
-        assertEquals(new Dollar(15),five.times(3));
+        Money five =  Money.dollar(5);
+        assertEquals(Money.dollar(10),five.times(2));
+        assertEquals(Money.dollar(15),five.times(3));
     }
     @Test
     public void testFrancMultiplication() {
@@ -28,10 +29,11 @@ class TddApplicationTests {
 
     @Test
     public void testEquality(){
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
-        assertFalse(new Dollar(5).equals(new Dollar(6)));
-        assertTrue(new Franc(5).equals(new Franc(5)));
-        assertFalse(new Franc(5).equals(new Franc(6)));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertTrue(Money.franc(5).equals(Money.franc(5)));
+        assertFalse(Money.franc(5).equals(Money.franc(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
 
     }
 
