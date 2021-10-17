148ed667283ccd9d194ab51c532f01fe4a1c1210
ys313 <ys3135@gmail.com>
2021-10-18 00:44:11 +0900

    15장 - 좀더 추상적인 선언을 통해 가지에서 뿌리(애초의 테스트 케이스)로 일반화 - 변경후(Expression fiveBucks)그 영향을 방은 다른 부분들을 변경하기 위해 컴파일러의 지시를 따랐다(Expression에 plus()를 추가하기)

--- src/main/java/com/example/tdd/Money/Expression.java
+++ src/main/java/com/example/tdd/Money/Expression.java
@@ -2,4 +2,6 @@ package com.example.tdd.Money;
 
 public interface Expression {
     Money reduce(Bank bank,String to);
+
+    Expression plus(Expression addend);
 }
--- src/main/java/com/example/tdd/Money/Money.java
+++ src/main/java/com/example/tdd/Money/Money.java
@@ -4,7 +4,7 @@ public  class Money implements Expression{
     protected int amount;
     protected String currency;
 
-    public Money times(int multiplier){
+    public Expression times(int multiplier){
         return new Money(amount*multiplier,currency);
     }
 
@@ -39,7 +39,7 @@ public  class Money implements Expression{
     }
 
 
-    public Expression plus(Money addend) {
+    public Expression plus(Expression addend) {
         return new Sum(this,addend);
     }
 
